023b45d0f2b476f6b478e0490096eb13b314c433
Alex Hicks <aldahick@iupui.edu>
2017-05-24 16:51:50 -0400

    Reimplement IHttpController.error and .result

--- server/api/interfaces/IHttpController.ts
+++ server/api/interfaces/IHttpController.ts
@@ -7,6 +7,22 @@ abstract class IHttpController extends IRequestHandler {
     public params: {[key: string]: string[]} = {};
     public raw: string[] = [];
     public routes: string[] = [];
+
+    public error(err: number, more?: {[key: string]: any}): void {
+        if (!more) {
+            more = {};
+        }
+        more["error"] = err;
+        this.res.raw = more;
+    }
+
+    public result(result: number, more?: {[key: string]: any}): void {
+        if (!more) {
+            more = {};
+        }
+        more["result"] = result;
+        this.res.raw = more;
+    }
 }
 
 export default IHttpController;

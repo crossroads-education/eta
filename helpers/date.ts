0851a177a22242f100d1dd06e3a6a745d0194400
Alex Hicks <aldahick@iupui.edu>
2017-06-07 11:28:23 -0400

    Reimplement HelperDate methods

--- helpers/date.ts
+++ helpers/date.ts
@@ -2,30 +2,53 @@ import * as moment from "moment";
 import HelperObject from "./object";
 
 export default class HelperDate {
+    public static Week: Week;
+    public static Month: Month;
     public static getDate(date: Date): Date {
         return new Date(moment(date).format("YYYY-MM-DD"));
     }
 
-    public static getMonthsBetween(start: Date, end: Date): number[] {
-        let months: number[] = [];
-        for (let i: number = start.getMonth(); i <= end.getMonth(); i++) {
-            months.push(i);
+    public static getMonthsBetween(start: Date, end: Date): Month[] {
+        let months: Month[] = [];
+        let startMonth: number = moment(start).month();
+        let endMonth: number = moment(end).month();
+        for (let month: number = startMonth; month < endMonth; month++) {
+            let start: moment.Moment = moment(month, "M");
+            let end: moment.Moment = moment(start).add(1, "month").subtract(1, "day");
+            months.push({
+                number: month,
+                name: start.format("MMMM"),
+                weeks: this.getWeeksBetween(start.toDate(), end.toDate())
+            });
         }
         return months;
     }
 
-    public static getWeeksBetween(start: Date, end: Date): {
-            number: number,
-            start: Date } [] {
-        let weeks: { number: number, start: Date }[] = [];
-        let interval: number = 1000 * 60 * 60 * 24 * 7; // 1 week
-        for (let i: number = start.getTime(); i <= end.getTime(); i += interval) {
-            let start: Date = new Date(i);
+    public static getWeeksBetween(start: Date, end: Date): Week[] {
+        let weeks: Week[] = [];
+        let startWeek: number = moment(start).week();
+        let endWeek: number = moment(end).week();
+        for (let week: number = startWeek; week < endWeek; week++) {
+            let start: moment.Moment = moment(week, "w");
+            let end: moment.Moment = moment(start).add(6, 'day');
             weeks.push({
-                number: moment(start).week(),
-                start: start
+                number: week,
+                start: start,
+                end: end
             });
         }
         return weeks;
     };
+}
+
+class Week {
+    public number: number;
+    public start: moment.Moment;
+    public end: moment.Moment;
+}
+
+class Month {
+    public number: number;
+    public name: string;
+    public weeks?: Week[];
 }

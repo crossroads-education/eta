0a06c674cb311ffb22f20b00e47010f28a10ac69
Alex Hicks <aldahick@iupui.edu>
2017-06-08 10:08:30 -0400

    Implement HelperNumber

--- helpers/index.ts
+++ helpers/index.ts
@@ -2,6 +2,7 @@
 export {default as crypto} from "./crypto";
 export {default as date} from "./date";
 export {default as fs} from "./fs";
+export {default as number} from "./number";
 export {default as object} from "./object";
 export {default as path} from "./path";
 export {default as session} from "./session";
---
+++ helpers/number.ts
@@ -0,0 +1,12 @@
+export default class HelperNumber {
+    /**
+     * Returns a random integer between min and max.
+     * If exclusive is true, the result will never be max.
+     * Borrowed (with modifications) from https://mzl.la/2qChqI1
+     */
+    public static getRandomInt(min: number, max: number, exclusive: boolean = false): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + (exclusive ? 0 : 1))) + min;
+    }
+}

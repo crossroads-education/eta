21334d9eb3648b85cf9bf36f5e502a394e6cf957
Alex Hicks <aldahick@iupui.edu>
2017-06-28 12:28:08 -0400

    Fix a bug in HelperString.toCamelCase()

--- helpers/string.ts
+++ helpers/string.ts
@@ -1,5 +1,8 @@
 export default class HelperString {
     public static toCamelCase(str: string): string {
+        if (str.length < 2) {
+            return !!str ? str.toLowerCase() : str;
+        }
         return str[0].toLowerCase() + str.substring(1);
     }
 }

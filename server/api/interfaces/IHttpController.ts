74241e10751cc40549fe420445fdbb77581e44ff
Alex Hicks <aldahick@iupui.edu>
2017-07-05 14:12:36 -0400

    Move helpers from IHttpController to IRequestHandler

--- server/api/interfaces/IHttpController.ts
+++ server/api/interfaces/IHttpController.ts
@@ -7,44 +7,6 @@ abstract class IHttpController extends IRequestHandler {
     public params: {[key: string]: string[]} = {};
     public raw: string[] = [];
     public routes: string[] = [];
-
-    public error(err: number, more?: {[key: string]: any}): void {
-        if (!more) {
-            more = {};
-        }
-        more["error"] = err;
-        this.res.raw = more;
-    }
-
-    public result(result: number, more?: {[key: string]: any}): void {
-        if (!more) {
-            more = {};
-        }
-        more["result"] = result;
-        this.res.raw = more;
-    }
-
-    public redirect(url: string): void {
-        this.res.redirect(url);
-        this.res.end();
-        this.res.finished = true;
-    }
-
-    public async saveSession(): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.req.session.save((err: Error) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
-
-    public isLoggedIn(): boolean {
-        return !!this.req.session && !!this.req.session.userid;
-    }
 }
 
 export default IHttpController;
--- server/api/interfaces/IRequestHandler.ts
+++ server/api/interfaces/IRequestHandler.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as helpers from "../../../helpers/index";
 
 abstract class IRequestHandler {
     public req: express.Request;
@@ -7,6 +8,36 @@ abstract class IRequestHandler {
 
     public constructor(init: Partial<IRequestHandler>) {
         Object.assign(this, init);
+    }
+
+    public error(err: number, more?: {[key: string]: any}): void {
+        if (!more) {
+            more = {};
+        }
+        more["error"] = err;
+        this.res.raw = more;
+    }
+
+    public result(result: number, more?: {[key: string]: any}): void {
+        if (!more) {
+            more = {};
+        }
+        more["result"] = result;
+        this.res.raw = more;
+    }
+
+    public redirect(url: string): void {
+        this.res.redirect(url);
+        this.res.end();
+        this.res.finished = true;
+    }
+
+    public async saveSession(): Promise<void> {
+        return helpers.session.save(this.req.session);
+    }
+
+    public isLoggedIn(): boolean {
+        return this.req.session !== undefined && this.req.session.userid !== undefined;
     }
 }
 

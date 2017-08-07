d9de3fe383fa9d049e5e36573bd98a324a2aa1e5
Alex Hicks <aldahick@iupui.edu>
2017-08-07 17:01:42 -0400

    Implement moving to the correct directory in get-module script

--- scripts/get_module.py
+++ scripts/get_module.py
@@ -6,15 +6,21 @@ from compile_client import compile_module as compile_client
 
 SERVER_DIR = utils.get_server_dir()
 
-def get_module(git_url, module_name):
+def get_module(git_url):
+    module_name = git_url.split("/")[-1].split(".git")[0]
     module_dir = SERVER_DIR + "modules/" + module_name
     if os.system("git clone %s %s" % (git_url, module_dir)) is not 0:
         print("Couldn't clone the repository. Please check that the Git URL exists and that your SSH key is valid.")
         return
+    module_config = json.loads(utils.read_file(module_dir + "/eta.json"))
+    if module_config["name"] != module_name:
+        module_name = module_config["name"]
+        old_module_dir = module_dir
+        module_dir = SERVER_DIR + "modules/" + module_name
+        os.system("mv %s %s" % (old_module_dir, module_dir))
     os.chdir(module_dir)
     os.system("npm i --only=dev")
     os.system("npm i --only=prod")
-    module_config = json.loads(utils.read_file(module_dir + "/eta.json"))
     for static_dir in module_config["staticDirs"]:
         js_dir = "%s/%s/js" % (module_dir, static_dir)
         if not os.path.isdir(js_dir):
@@ -30,15 +36,8 @@ def get_module(git_url, module_name):
     compile_client(module_name)
 
 def main():
-    git_url = None
-    module_name = None
-    if len(sys.argv) == 3:
-        git_url = sys.argv[1]
-        module_name = sys.argv[2]
-    else:
-        git_url = utils.get_input("Git URL: ")
-        module_name = utils.get_input("Module name: ")
-    get_module(git_url, module_name)
+    git_url = sys.argv[1] if len(sys.argv) >= 2 else utils.get_input("Git URL: ")
+    get_module(git_url)
 
 if __name__ == "__main__":
     main()

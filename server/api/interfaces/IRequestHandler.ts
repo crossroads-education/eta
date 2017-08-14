4cae5231e520072a9865ff0d57678163ecbf96de
Alex Hicks <aldahick@iupui.edu>
2017-08-14 15:38:11 -0400

    Fix redirect parameter order

--- server/api/interfaces/IRequestHandler.ts
+++ server/api/interfaces/IRequestHandler.ts
@@ -27,7 +27,7 @@ abstract class IRequestHandler {
     }
 
     public redirect(url: string): void {
-        this.res.redirect(url, 307);
+        this.res.redirect(307, url);
         this.res.end();
         this.res.finished = true;
     }

3f56a3fd46491ea1600756c3431f904ae69b28b7
Alex Hicks <aldahick@iupui.edu>
2017-09-15 11:44:34 -0400

    Implement HelperString.toLowerCamelCase()

--- helpers/string.ts
+++ helpers/string.ts
@@ -5,4 +5,17 @@ export default class HelperString {
         }
         return str[0].toLowerCase() + str.substring(1);
     }
+
+    public static toLowerCamelCase(str: string): string {
+        let result = "";
+        let i: number;
+        for (i = 0; i < str.length; i++) {
+            if (str[i] === str[i].toLowerCase()) {
+                break;
+            }
+            result += str[i].toLowerCase();
+        }
+        result += str.slice(i);
+        return result;
+    }
 }

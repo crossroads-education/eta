b85338d64d0c4d3200b34dd9e87e1a7fd99ddde6
Alex Hicks <aldahick@iupui.edu>
2017-11-15 15:19:42 -0500

    Add Lodash export to `eta.ts`

--- eta.ts
+++ eta.ts
@@ -1,4 +1,9 @@
 /// <reference path="./def/express.d.ts" />
+import * as lodash from "lodash";
+/**
+ * Link to the "underscore" library for convenience
+ */
+export const _ = lodash;
 export * from "./helpers/index";
 export * from "./server/api/index";
 export * from "./lib/index";

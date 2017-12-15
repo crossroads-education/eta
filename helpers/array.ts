4c3567acab8fbf0ddfd8cf76efe4411c85b56fa5
Alex Hicks <alex@alexhicks.net>
2017-12-15 17:05:53 -0500

    Implement `HelperArray.mapObject()`

--- helpers/array.ts
+++ helpers/array.ts
@@ -1,5 +1,17 @@
 export default class HelperArray {
     /**
+     * Maps an array into an object.
+     * @param arr Array to build object from: [label: string, result: U]
+     * @return Transformed object
+     */
+    public static mapObject<T>(pairs: [string, T][]): {[key: string]: T} {
+        const object: {[key: string]: T} = {};
+        pairs.forEach(pair => {
+            object[pair[0]] = pair[1];
+        });
+        return object;
+    }
+    /**
      * DEPRECATED: Use Array.filter()
      * Removes elements from `arr` which return true when passed to `selector`.
      * @param arr Array to remove elements from

d419140be8aab6c8a0061c5ae29ea69b1997c0ac
Alex Hicks <alex@alexhicks.net>
2018-01-12 14:26:19 -0500

    Improve `HelperArray.mapObject()`

--- helpers/array.ts
+++ helpers/array.ts
@@ -4,10 +4,11 @@ export default class HelperArray {
      * @param arr Array to build object from: [label: string, result: U]
      * @return Transformed object
      */
-    public static mapObject<T>(pairs: [string, T][]): {[key: string]: T} {
+    public static mapObject<T>(pairs: (string | T)[][]): {[key: string]: T} {
         const object: {[key: string]: T} = {};
         pairs.forEach(pair => {
-            object[pair[0]] = pair[1];
+            const key = <string>pair[0];
+            object[key] = <T>pair[1];
         });
         return object;
     }

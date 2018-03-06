82def1c85f4f21eec88c9a7cb7a618b54b7cce49
Alex Hicks <alex@alexhicks.net>
2018-03-06 09:35:58 -0500

    Refactor TestRunner to be less imposing

--- lib/TestRunner.ts
+++ lib/TestRunner.ts
@@ -4,56 +4,29 @@ import * as supertest from "supertest";
 
 export default class TestRunner {
     private agent: supertest.SuperTest<supertest.Test>;
+    private pathPrefix: string;
 
-    public constructor(url = "http://localhost:3000") {
-        this.agent = supertest.agent(url);
+    public constructor(baseUrl = "http://localhost:3000", pathPrefix = "") {
+        this.agent = supertest.agent(baseUrl);
+        this.pathPrefix = pathPrefix;
     }
 
-    public describeEndpoint(url: string, tests: {[key: string]: (res: supertest.Response) => Promise<void>}, options?: Partial<DescribeOptions>): void {
-        options = _.defaults<Partial<DescribeOptions>, DescribeOptions>(options, {
-            transformRequest: req => req,
+    public request(url: string, options?: Partial<RequestOptions>): supertest.Test {
+        url = this.pathPrefix + url;
+        options = _.defaults<Partial<RequestOptions>, Partial<RequestOptions>>(options, {
             method: "get",
             apiToken: process.env.API_TOKEN,
             params: {}
         });
-        describe(options.method.toUpperCase() + " " + url, () => {
-            let res: supertest.Response;
-            beforeEach(done => {
-                let req = options.transformRequest(this.agent[options.method].bind(this.agent)(url)
-                    .set("Authorization", "Bearer " + options.apiToken));
-                if (Object.keys(options.params).length > 0) {
-                    if (options.method === "get") {
-                        req = req.query(options.params);
-                    } else {
-                        req = req.send(options.params).type("form");
-                    }
-                }
-                req.then(_res => {
-                    res = _res;
-                    done();
-                }).catch(done);
-            });
-            it("should respond successfully", () => {
-                expect(res.status).to.equal(200);
-            });
-            it("should respond with JSON", () => {
-                expect(res.header).to.have.property("content-type").which.matches(/^application\/json/);
-            });
-            it("should have successful result", () => {
-                expect(res.body).to.have.property("result");
-                expect(res.body.result).to.equal(0);
-            });
-            Object.keys(tests).forEach(k => {
-                it(k, done => {
-                    tests[k](res).then(done).catch(done);
-                });
-            });
-        });
+        let req: supertest.Test = this.agent[options.method].bind(this.agent)(url);
+        if (options.apiToken !== undefined) req = req.set("Authorization", "Bearer " + options.apiToken);
+        if (options.method === "get") req = req.query(options.params);
+        else req = req.send(options.params).type("form");
+        return req;
     }
 }
 
-interface DescribeOptions {
-    transformRequest: (req: supertest.Test) => supertest.Test;
+interface RequestOptions {
     method: "get" | "post" | "patch" | "put" | "delete";
     apiToken: string;
     params: {[key: string]: any};

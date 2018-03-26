3d26c77bd3951c2eec9cb86749b03654bc6353e1
Alex Hicks <alex@alexhicks.net>
2018-03-26 14:24:44 -0400

    Remove outdated `HelperObject.merge()`

--- helpers/object.ts
+++ helpers/object.ts
@@ -2,24 +2,6 @@ import HelperArray from "./array";
 
 export default class HelperObject {
     /**
-     * DEPRECATED
-     * Only kept for unique `reverseArrays` behavior
-     * @deprecated
-     */
-    public static merge<T>(from: T, to: T, reverseArrays = false): T {
-        for (const i in from) {
-            if (from[i] instanceof Array && to[i] instanceof Array) {
-                to[i] = reverseArrays ? (<any>from[i]).concat(to[i]) : (<any>to[i]).concat(from[i]);
-            } else if (typeof(from[i]) === "object" && typeof(to[i]) === "object") {
-                to[i] = this.merge(from[i], to[i], reverseArrays);
-            } else {
-                to[i] = from[i];
-            }
-        }
-        return to;
-    }
-
-    /**
      * Converts an enum (mapping of string <-> number) to a mapping string -> number.
      */
     public static enumToPure(obj: any): {[key: string]: number} {
--- test/helpers/object.test.ts
+++ test/helpers/object.test.ts
@@ -2,39 +2,7 @@ import { expect } from "chai";
 import HelperObject from "../../helpers/object";
 
 describe("helpers/object", () => {
-    describe("#merge()", () => {
-        it("should combine properties of `from` and `to`", () => {
-            const from: any = { foo: true };
-            const to: any = { bar: 4 };
-            const expected: any = {
-                bar: 4,
-                foo: true
-            };
-            expect(HelperObject.merge(from, to)).to.deep.equal(expected);
         });
-        it("should overwrite non-array members of `to` with `from`", () => {
-            const from: any = { foo: true };
-            const to: any = { foo: false };
-            const expected: any = { foo: true };
-            expect(HelperObject.merge(from, to)).to.deep.equal(expected);
-        });
-        it("should concatenate array members of `to` to `from`", () => {
-            const from: any = { foo: [2, 3] };
-            const to: any = { foo: [1] };
-            const expected: any = { foo: [1, 2, 3] };
-            expect(HelperObject.merge(from, to)).to.deep.equal(expected);
-        });
-        it("should concatenate array members of `from` to `to` given `reverseArrays` parameter", () => {
-            const from: any = { foo: [2, 3] };
-            const to: any = { foo: [1] };
-            const expected: any = { foo: [2, 3, 1] };
-            expect(HelperObject.merge(from, to, true)).to.deep.equal(expected);
-        });
-        it("should return the same reference as `to`", () => {
-            const from: any = { foo: 2 };
-            const to: any = { bar: 3 };
-            const result: any = HelperObject.merge(from, to);
-            expect(to).to.equal(result);
         });
     });
 });

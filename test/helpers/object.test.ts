25e1dfdbb5fdb27d01c8bdc335f51535713231c2
Alex Hicks <alex@alexhicks.net>
2018-03-26 14:25:06 -0400

    Add unit tests for all HelperObject methods

--- test/helpers/object.test.ts
+++ test/helpers/object.test.ts
@@ -2,7 +2,23 @@ import { expect } from "chai";
 import HelperObject from "../../helpers/object";
 
 describe("helpers/object", () => {
+    describe("#enumToPure()", () => {
+        enum Foo { A, B }
+        it("should convert an enum to pure", () => {
+            const pure = HelperObject.enumToPure(Foo);
+            expect(pure).to.have.property("A")
+                .which.equals(Foo.A);
+            expect(pure).to.have.property("B")
+                .which.equals(Foo.B);
+            expect(pure).to.not.have.property(Foo.A.toString());
+            expect(pure).to.not.have.property(Foo.B.toString());
         });
+    });
+
+    describe("#getFunctionParameterNames()", () => {
+        function foo(a: number, b: string) { }
+        it("should get function parameters correctly", () => {
+            expect(HelperObject.getFunctionParameterNames(foo)).to.deep.equal(["a", "b"]);
         });
     });
 });

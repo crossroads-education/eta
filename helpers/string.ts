8ca31b61e8f9e1a961c779e88b7087905f493b80
Alex Hicks <alex@alexhicks.net>
2018-04-16 11:54:18 -0400

    Remove `HelperString.toCamelCase()` (replace with `_.camelCase()`)

--- helpers/string.ts
+++
@@ -1,8 +0,0 @@
-export default class HelperString {
-    public static toCamelCase(str: string): string {
-        if (str.length < 2) {
-            return !!str ? str.toLowerCase() : str;
-        }
-        return str[0].toLowerCase() + str.substring(1);
-    }
-}

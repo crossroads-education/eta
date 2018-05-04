0868cba1a3f7f09c32c353e39fca3b665512cad2
Alex Hicks <alex@alexhicks.net>
2018-05-04 06:57:06 -0400

    Use `__dirname` instead of `process.cwd()` for basePath

--- server/api/constants.ts
+++ server/api/constants.ts
@@ -1,5 +1,7 @@
+import * as path from "path";
+
 export default class Constants {
-    public static basePath: string = process.cwd().replace(/\\/g, "/") + "/";
+    public static basePath: string = path.resolve(__dirname, "../..").replace(/\\/g, "/") + "/";
     public static controllerPaths: string[];
     public static modulesPath: string = Constants.basePath + "modules/";
     public static staticPaths: string[];

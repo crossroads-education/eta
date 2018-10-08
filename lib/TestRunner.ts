c75001b690928508d7f9f88faa11e8d25c63dfc9
Erik Weitnauer <eweitnauer@gmail.com>
2018-10-08 14:49:52 -0400

    new mechanism to authorize as a user in test files

--- lib/TestRunner.ts
+++ lib/TestRunner.ts
@@ -4,29 +4,36 @@ import * as supertest from "supertest";
 export default class TestRunner {
     private agent: supertest.SuperTest<supertest.Test>;
     private pathPrefix: string;
+    private apiToken: string;
 
-    public constructor(baseUrl = "http://localhost:3000", pathPrefix = "") {
+    public constructor(baseUrl = "http://localhost:3000", pathPrefix = "", apiToken = "ari5END35D__53") {
         this.agent = supertest.agent(baseUrl);
         this.pathPrefix = pathPrefix;
+        this.apiToken = apiToken;
     }
 
+    // Pass a userId in the options to authorize as this user.
     public request(url: string, options?: Partial<RequestOptions>): supertest.Test {
         if (!url.startsWith("/")) url = this.pathPrefix + url;
         options = _.defaults<Partial<RequestOptions>, Partial<RequestOptions>>(options, {
             method: "get",
-            apiToken: process.env.API_TOKEN,
+            userId: undefined,
             params: {}
         });
         let req: supertest.Test = this.agent[options.method].bind(this.agent)(url);
-        if (options.apiToken !== undefined) req = req.set("Authorization", "Bearer " + options.apiToken);
+        req.set("x-requested-with", "XMLHttpRequest"); // tells eta to not forward to a login page
+        if (options.userId !== undefined) {
+            req.set("Authorization", `Basic ${options.userId}:${this.apiToken}`);
+        }
         if (options.method === "get") req = req.query(options.params);
-        else req = req.send(options.params).type("form");
+        // this seems to be a problem => it turns numbers into strings
+        else req = req.send(options.params);
         return req;
     }
 }
 
 interface RequestOptions {
     method: "get" | "post" | "patch" | "put" | "delete";
-    apiToken: string;
+    userId: number;
     params: {[key: string]: any};
 }
